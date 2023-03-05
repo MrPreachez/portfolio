24baf9aa7727858becdf9c20d3ed93737bc9f175
Damon  Chouinard <fengchoui@gmail.com>
2023-03-04 16:07:46 -0800

    basic update, rough text

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,7 +1,18 @@
 import "./AboutPage.scss";
 
 function AboutPage() {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        At this point in time I live in Winlaw BC, 45 mins outside of Nelson.
+        I'm a father, stepfather and partner with strong roots in food and
+        agriculture. Creative processes, recreating in nature and many things
+        tech(primarily software dev) are what excite me these days. I'm a doer
+        that loves entrepreneurship, collaboration, regenerative agriculture and
+        cultivating collective experiences.   '{" "}
+      </p>
+    </div>
+  );
 }
 
 export default AboutPage;
