d894247ea3ba396b81bbb457234015f1142bd370
Damon  Chouinard <fengchoui@gmail.com>
2023-03-08 20:56:27 -0800

    functional setup and initial styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,29 @@
-import "./Header.scss"
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 function Header() {
-    return (
-        <div>
+  return (
+    <div className="nav__section">
+        <div className="nav__limit">
+      <Link to="/" className="nav__link nav__link--logo">
+        {" "}
+        <h2 className="nav__logo">DC</h2>
+      </Link>
+      <div className="nav__container">
+        <div className="nav__links--wrapper">
+          <Link to="/about" className="nav__link">
+            <h4 className="nav__about">About</h4>
+          </Link>
+          <Link to="/contact" className="nav__link">
+            <h4 className="nav__contact">Contact</h4>
+          </Link>
 
+          <button className="nav__resumeBtn">Resume</button>
         </div>
-    )
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
