90ba2b2d4229132ed0046511f6abc11f0cc69b25
Damon  Chouinard <fengchoui@gmail.com>
2023-03-14 15:25:22 -0700

    added react spring and did a quick edit on footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,22 +10,31 @@ function Footer() {
     <div className="footer__section">
       <div className="footer__container">
         <div className="footer__block1">
-          <Link to="https://github.com/MrPreachez">
-            <img
-              src={github}
-              alt="github icon"
-              className="footer__github-icon"
-            />
+          <Link to="https://github.com/MrPreachez" className="footer__link">
+            <div className="footer__icon-block">
+              <img
+                src={github}
+                alt="github icon"
+                className="footer__github-icon footer__icon"
+              />
+              <p>Github</p>
+            </div>
           </Link>
-          <Link to="https://www.linkedin.com/in/damon-chouinard/">
-            <img src={linkedIn} alt="linkedin icon" />
+          <Link to="https://www.linkedin.com/in/damon-chouinard/" className="footer__link">
+            <div className="footer__icon-block">
+              <img src={linkedIn} alt="linkedin icon" className="footer__icon" />
+              <p>LinkedIn</p>
+            </div>
           </Link>
-          <Link className="footer__email--link" to="/contact">
-            <img
-              src={envelope}
-              alt="envelope icon"
-              className="footer__email-icon"
-            />
+          <Link className="footer__email--link footer__link" to="/contact">
+            <div className="footer__icon-block">
+              <img
+                src={envelope}
+                alt="envelope icon"
+                className="footer__email-icon footer__icon"
+              />
+              <p>Email</p>
+            </div>
           </Link>
         </div>
         <div className="footer__block2">
