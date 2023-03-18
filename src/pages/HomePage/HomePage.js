1217b514341502e7c4ce371542f141beeab60f73
Damon  Chouinard <fengchoui@gmail.com>
2023-03-17 23:22:54 -0700

    animation on scroll

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,22 +1,56 @@
 import "./HomePage.scss";
 import headshot from "../../assets/images/portfolio-copy.jpg";
-import { useSpring, animated as a } from "@react-spring/web";
+import { useSpring, useInView, animated as a } from "@react-spring/web";
 
 function HomePage() {
   const spring1 = useSpring({
     from: { opacity: 0, x: 200 },
     to: { opacity: 1, x: 0 },
-    config: { duration: 700 },
+    config: {  mass: 4, friction: 100, tension: 200  },
   });
   const spring2 = useSpring({
-    from: { opacity: 0 }, opacity: 1,
-    config: { duration: 1000 }
-  });  
+    from: { opacity: 0, x: -300 },
+    to: { opacity: 1, x: 0 },
+    config: {  mass: 4, friction: 100, tension: 200 },
+  });
   const spring3 = useSpring({
     from: { opacity: 0, y: 200 },
     to: { opacity: 1, y: 0 },
-    config: { duration: 700 },
+    config: { mass: 5, friction: 100, tension: 200 },
   });
+  const [ref, springs4] = useInView(() => ({
+    from: {
+      opacity: 0,
+      x: -200,
+    },
+    to: {
+      opacity: 1,
+      x: 0,
+      config: {
+        mass: 5,
+        friction: 100,
+        tension: 300,
+      },
+    },
+    
+  }));
+  const [ref1, springs5] = useInView(() => ({
+    from: {
+      opacity: 0,
+      y: 200,
+    },
+    to: {
+      opacity: 1,
+      y: 0,
+      config: {
+        mass: 1,
+        friction: 50,
+        tension: 300,
+      },
+    },
+    
+  }));
+  
 
   return (
     <main className="homePage__body">
@@ -25,7 +59,9 @@ function HomePage() {
           <a.h1 style={spring1} className="head__title1">
             DAMON
           </a.h1>
-          <a.h2 style={spring2} className="head__title2">CHOUINARD</a.h2>
+          <a.h2 style={spring2} className="head__title2">
+            CHOUINARD
+          </a.h2>
         </div>
         <div className="head__description-position">
           <h3 className="head__description-title">Software Developer...</h3>
@@ -84,14 +120,20 @@ function HomePage() {
           <span className="about__feature" />
         </h2>
         <div className="about__container--wrapper">
-          <p className="about__text">
+          <a.p ref={ref} style={springs4} className="about__text">
             Born and raised in BC, I grew up just outside of Vancouver. I
             currently live, work and play in the Socan Valley, just outside of
             Nelson. I was brought here by a calling and opportunity to live
             rurally and become a farmer...
-          </p>
+          </a.p>
           <div className="about__imgContainer">
-            <img src={headshot} className="about__imgPhoto" alt="alt-text" />
+            <a.img
+              ref={ref1}
+              style={springs5}
+              src={headshot}
+              className="about__imgPhoto"
+              alt="alt-text"
+            />
           </div>
         </div>
       </section>
