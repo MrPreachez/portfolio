83137397603192c309e62378111a885b76e38bde
Damon  Chouinard <fengchoui@gmail.com>
2023-03-18 23:16:32 -0700

    melder page inputs

--- src/pages/ProjectMelder/ProjectMelder.js
+++ src/pages/ProjectMelder/ProjectMelder.js
@@ -8,20 +8,29 @@ function ProjectMelder() {
         <section className="melder__hero">
           <div className="melder__hero-container">
             <h1 className="melder__title">Project: Melder</h1>
-            <div>
+            <div className="melder__hero--responsive">
               <img className="melder__heroImage" src={melder} />
-              <h3 className="">
+              <p className="melder__hero-aside">
                 Melder is a fullstack qualitative survey analysis tool. It's
                 inspired by my experiences with community engagement, and an
                 interest in AI. It's built with React, Node.js, Express, MySQL
-                and the davinci-003 OpenAi API.{" "}
-              </h3>
+                and the davinci-003 OpenAi API.
+              </p>
             </div>
           </div>
         </section>
         <section className="melder__overview">
           <h4>Project Overview</h4>
-          <p>hello</p>
+          <p>
+            The objective with Melder was to create a fullstack web application
+            that had real world relevance to my personal experiences. From my
+            community development projects and event management work, I've had
+            plenty of occassions where i've had to make sense of group
+            sentiment. I decided to use this opportunity to create an
+            application that utilized the OpenAi api to summarize qualitative
+            inputs and offer useful feedback to the user group.{" "}
+            T
+          </p>
         </section>
         <section className="melder__details"></section>
       </main>
